78d4680d7b7100365a3a9b8501ef8cbb969e577c
Chufeng JIANG <jiangchufengjcf@163.com>
2023-10-31 18:50:20 -0700

    Server OK1

---
+++ ServerSeckey/ServerSeckey/ServerOP.cpp
@@ -0,0 +1,249 @@
+﻿#include "ServerOP.h"
+#include "TcpSocket.h"
+#include "RequestFactory.h"
+#include "RequestCodec.h"
+#include "RespondCodec.h"
+#include "RespondFactory.h"
+#include "RsaCrypto.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <json/json.h> //这里要写Linux目录/usr/lib目录中json存放的位置，因为linux在搜索头文件时只会搜索到/usr/lib，不会主动继续深入搜索/usr/lib/json
+#include <unistd.h>
+#include "Hash.h"
+using namespace std;
+using namespace Json;
+
+/*
+	{
+		"Port":9898
+	}
+*/
+
+// 参数由main.cpp传过来，为json文件的名字，放在工程文件目录下
+ServerOP::ServerOP(string jsonFile)
+{
+	// 解析json文件, 读文件 -> Value
+	ifstream ifs(jsonFile);
+	Reader r;
+	Value root;
+	r.parse(ifs, root);
+	// 将root中的键值对value值取出
+	m_serverID = root["ServerID"].asString();
+	m_port = root["Port"].asInt();
+	// 数据库相关的信息
+	m_dbUser = root["UserDB"].asString();
+	m_dbPwd = root["PwdDB"].asString();
+	m_dbConnStr = root["ConnStrDB"].asString();
+
+	// 实例化一个连接oracle数据的对象
+	m_occi.connectDB(m_dbUser, m_dbPwd, m_dbConnStr);
+
+	// 实例化共享内存对象
+	// 从配置文件中读 key/pathname
+	string shmKey = root["ShmKey"].asString();
+	int maxNode = root["ShmMaxNode"].asInt();
+	// 客户端存储的秘钥只有一个
+	m_shm = new SecKeyShm(shmKey, maxNode);
+}
+
+
+void ServerOP::startServer()
+{
+	m_server = new TcpServer;
+	m_server->setListen(m_port);
+	while (1)
+	{
+		cout << "等待客户端连接..." << endl;
+		TcpSocket* tcp = m_server->acceptConn();
+		if (tcp == NULL)
+		{
+			continue;
+		}
+		cout << "与客户端连接成功..." << endl;
+		// 通信
+		pthread_t tid;
+		// 这个回调可以是类的静态函数, 类的友元函数, 普通的函数
+		// 友元的类的朋友, 但是不属于这个类
+		// 友元函数可以访问当前类的私有成员
+		pthread_create(&tid, NULL, workHard, this);
+		
+		/*
+		* pthread_create是C的API，不属于任何类，working要是一个实例化的对象，才能用在第三个参数。
+		working在这里不能用，因为working是一个类成员，类成员要实例化对象才能用，通过类对象去调用这个成员。
+		方案一：
+		pthread_create是C的API，要求这个函数任何时候都能用，因此需要设置working函数为静态的。
+		方案二：
+		设置友元workHard。
+
+		*/
+		//pthread_create(&tid, NULL, working, this);
+		
+		m_list.insert(make_pair(tid, tcp));
+	}
+}
+
+void * ServerOP::working(void * arg)
+{
+	return nullptr;
+}
+
+// 返回值为string类型
+string ServerOP::seckeyAgree(RequestMsg* reqMsg)
+{
+	// 0. 对签名进行校验 -> 公钥解密 -> 得到公钥
+	// 将收到的公钥数据写入本地磁盘
+	ofstream ofs("public.pem");
+	ofs << reqMsg->data();
+	ofs.close();
+	// 创建非对称加密对象
+	RespondInfo info;
+	RsaCrypto rsa("public.pem", false);
+
+	// 创建哈希对象
+	Hash sha(T_SHA1);
+	sha.addData(reqMsg->data());
+	cout << "1111111111111111" << endl;
+	bool bl = rsa.rsaVerify(sha.result(), reqMsg->sign());
+	cout << "00000000000000000000" << endl;
+	if (bl == false)
+	{
+		cout << "签名校验失败..." << endl;
+		info.status = false;
+	}
+	else
+	{
+		cout << "签名校验成功..." << endl;
+		// 1. 生成随机字符串
+		//   对称加密的秘钥, 使用对称加密算法 aes, 秘钥长度: 16, 24, 32byte
+		string key = getRandKey(Len16);
+		cout << "生成的随机秘钥: " << key << endl;
+		// 2. 通过公钥加密
+		cout << "aaaaaaaaaaaaaaaa" << endl;
+		string seckey = rsa.rsaPubKeyEncrypt(key);
+		cout << "加密之后的秘钥: " << seckey << endl;
+		// 3. 初始化回复的数据
+		info.clientID = reqMsg->clientid();
+		info.data = seckey;
+		info.serverID = m_serverID;
+		info.status = true;	
+
+		// 将生成的新秘钥写入到数据库中 -> 操作 SECKEYINFO
+		NodeSecKeyInfo node;
+		strcpy(node.clientID, reqMsg->clientid().data());
+		strcpy(node.serverID, reqMsg->serverid().data());
+		strcpy(node.seckey, key.data());
+		node.seckeyID = m_occi.getKeyID();	// 秘钥的ID
+		info.seckeyID = node.seckeyID;
+		node.status = 1;
+		// 初始化node变量
+		bool bl = m_occi.writeSecKey(&node);
+		if(bl)
+		{
+			// 成功
+			m_occi.updataKeyID(node.seckeyID + 1);
+			// 写共享内存
+			m_shm->shmWrite(&node);
+		}
+		else
+		{
+			// 失败
+			info.status = false;
+		}
+	}
+
+	// 4. 序列化
+	CodecFactory* fac = new RespondFactory(&info);
+	Codec* c = fac->createCodec();
+	string encMsg = c->encodeMsg();
+	// 5. 发送数据
+	return encMsg;
+
+}
+
+ServerOP::~ServerOP()
+{
+	if (m_server)
+	{
+		delete m_server;
+	}
+	delete m_shm;
+}
+
+// 要求: 字符串中包含: a-z, A-Z, 0-9, 特殊字符
+string ServerOP::getRandKey(KeyLen len)
+{
+	// 设置随机数数种子 => 根据时间
+	srand(time(NULL));
+	int flag = 0;
+	string randStr = string();
+
+	//char* cs = "~!@#$%^&*()_+}{|\';[]";
+	char *cs = "~!@#$%^&*()_+}{|\';[]";
+	for (int i = 0; i < len; ++i)
+	{
+		flag = rand() % 4;	// 4中字符类型
+		switch (flag)
+		{
+		case 0:	// a-z
+			randStr.append(1, 'a' + rand() % 26);
+			break;
+		case 1: // A-Z
+			randStr.append(1, 'A' + rand() % 26);
+			break;
+		case 2: // 0-9
+			randStr.append(1, '0' + rand() % 10);
+			break;
+		case 3: // 特殊字符
+			randStr.append(1, cs[rand() % strlen(cs)]);
+			break;
+		default:
+			break;
+		}
+	}
+	return randStr;
+}
+
+// 通信的套接字对象在这里
+void* workHard(void * arg)
+{
+	//主线程已经创建对象了，子线程要开始取TCP对象，但是主线程还没有进行插入m_list.insert(make_pair(tid, tcp));会造成两个线程不同步
+	// sleep不稳定，但是可行；或者添加一个互斥锁，共享资源是mlist，锁mlist就行。 
+	sleep(1);
+	string data = string(); //用于接收传递秘钥
+	// 通过参数将传递的this对象转换回原来的数据类型
+	ServerOP* op = (ServerOP*)arg;
+	// 从op中将通信的套接字对象取出
+	TcpSocket* tcp = op->m_list[pthread_self()];
+	// 1. 接收客户端数据 -> 编码
+	string msg = tcp->recvMsg();
+	// 2. 反序列化 -> 得到原始数据 RequestMsg 类型
+	CodecFactory* fac = new RequestFactory(msg);
+	Codec* c = fac->createCodec();
+	RequestMsg* req = (RequestMsg*)c->decodeMsg();
+	// 3. 取出数据
+	// 判断客户端是什么请求
+	switch (req->cmdtype())
+	{
+	case 1:
+		// 秘钥协商
+		data = op->seckeyAgree(req);
+		break;
+	case 2:
+		// 秘钥校验
+		break;
+	default:
+		break;
+	}
+
+	// 释放资源
+	delete fac;
+	delete c;
+	// tcp对象如何处理
+	tcp->sendMsg(data);
+	tcp->disConnect();
+	op->m_list.erase(pthread_self());
+	delete tcp;
+
+	return NULL;
+}
---
+++ ServerSeckey/ServerSeckey/ServerOP.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <map>
+#include "TcpServer.h"
+#include "Message.pb.h"
+#include "OCCIOP.h"
+#include "SecKeyShm.h"
+// 处理客户端请求
+class ServerOP
+{
+public:
+	enum KeyLen {Len16=16, Len24=24, Len32=32};
+	ServerOP(string json);
+	// 启动服务器
+	void startServer();
+	// 线程工作函数 -> 推荐使用，但是本案例使用友元
+	static void* working(void* arg);
+	// 友元破坏了类的封装
+	friend void* workHard(void* arg);
+	// 秘钥协商
+	string seckeyAgree(RequestMsg* reqMsg);
+	~ServerOP();
+
+private:
+	string getRandKey(KeyLen len);
+
+private:
+	string m_serverID;	// 当前服务器的ID
+	string m_dbUser;
+	string m_dbPwd;
+	string m_dbConnStr;
+	unsigned short m_port;
+	map<pthread_t, TcpSocket*> m_list;
+	TcpServer *m_server = NULL;
+	// 创建数据库实例对象，因为在多处进行了使用，因此设置为成员
+	OCCIOP m_occi;
+	SecKeyShm* m_shm;
+};
+
+void* workHard(void* arg);
+
